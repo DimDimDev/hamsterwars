6d45f19f51edc92962f7794bf6ac300df66c91cd
DimDimDev <dimitris.adabugday@gmail.com>
2020-05-12 11:56:35 +0200

    updated games

--- routes/charts.js
+++ routes/charts.js
@@ -3,12 +3,41 @@ const { db } = require('../firebase')
 
 let router = new Router();
 
-// GET /charts/top
 //Returnerar en array med top 5 mest vinnande hamsterobject
+router.get('/top', async (req, res) => {
+    try {
+        let hamstersArray = [];
+        let hamstersDB = await db.collection('hamsters').orderBy('wins', 'desc').limit(5).get();
+
+        hamstersDB.forEach(hamster => {
+            hamstersArray.push(hamster.data());
+        })
+
+        res.status(200).send(hamstersArray)
+
+    } catch (err) {
+        res.status(500)
+        console.error(err)
+    }
+})
 
 
-// GET /charts/bottom
 //Returnerar en array med top 5 mest förlorande hamsterobject.
+router.get('/bottom', async (req, res) => {
+    try {
+        let hamstersArray = [];
+        let hamstersDB = await db.collection('hamsters').orderBy('defeats', 'desc').limit(5).get();
 
+        hamstersDB.forEach(hamster => {
+            hamstersArray.push(hamster.data());
+        })
+
+        res.status(200).send(hamstersArray)
+
+    } catch (err) {
+        res.status(500)
+        console.error(err)
+    }
+})
 
 module.exports = router;
--- routes/games.js
+++ routes/games.js
@@ -3,12 +3,42 @@ const { db } = require('../firebase')
 
 let router = new Router();
 
-// GET /games
-//Returnerar en array med samtliga matchobject som hållits.
+//Returns all games
+router.get('/', async (req, res) => {
+    try {
+        let gamesArray = [];
+        let gamesDB = await db.collection('games').get();
+
+        gamesDB.forEach(game => {
+            gamesArray.push(game.data());
+        })
+
+        res.status(200).send(gamesArray)
+
+    } catch (err) {
+        res.status(500)
+        console.error(err)
+    }
+})
 
 
-// POST /games
-//Sparar en match med formatet { timeStamp: Date, contestants: [{ hamsterobject }, { hamsterobject } ] }.
+//Saves a game
+router.post('/', async (req, res) => {
+    try {
+        let match = {
+            timeStamp: new Date(),
+            contestants: [req.body.contestant1, req.body.contestant2],
+            winner: req.body.winner
+        }
+
+        await db.collection('games').doc().set(match);
+        res.status(200).send("New match added to games")
+
+    } catch (err) {
+        res.status(500)
+        console.error(err)
+    }
+})
 
 
 module.exports = router;
