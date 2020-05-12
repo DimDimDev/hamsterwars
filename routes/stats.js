57da60ec1fb0b29d55f4164aa67d691af352c645
DimDimDev <dimitris.adabugday@gmail.com>
2020-05-12 14:41:10 +0200

    updated stats

--- routes/stats.js
+++ routes/stats.js
@@ -4,11 +4,89 @@ const { db } = require('../firebase')
 let router = new Router();
 
 
-// GET /stats/total
-//Returnerar ett statsobject med totalt antal matcher som hållits.
+//Returns total of games played
+router.get('/total', async (req, res) => {
+    try {
+        let gamesArray = [];
+        let gamesDB = await db.collection('games').get();
+
+        gamesDB.forEach(game => {
+            gamesArray.push(game.data());
+        })
+
+        res.status(200).send({ totalGames: gamesArray.length, gamesArray })
+
+    } catch (err) {
+        res.status(500)
+        console.error(err)
+    }
+})
 
 
-// GET /stats/{opt}
-//Känner er fria att sammanställa annan spännande statistik, ex. hur många % gillar majs? Vad är medelåldern på samtliga hamstrar, etc.
+//Returns the ten oldest hamsters
+router.get('/oldest', async (req, res) => {
+    try {
+        let hamstersArray = [];
+        let hamstersDB = await db.collection('hamsters').orderBy('age', 'desc').limit(10).get();
+
+        hamstersDB.forEach(hamster => {
+            hamstersArray.push(hamster.data());
+        })
+
+        res.status(200).send(hamstersArray)
+
+    } catch (err) {
+        res.status(500)
+        console.error(err)
+    }
+})
+
+
+//Returns the ten youngest hamsters
+router.get('/youngest', async (req, res) => {
+    try {
+        let hamstersArray = [];
+        let hamstersDB = await db.collection('hamsters').orderBy('age', 'asc').limit(10).get();
+
+        hamstersDB.forEach(hamster => {
+            hamstersArray.push(hamster.data());
+        })
+
+        res.status(200).send(hamstersArray)
+
+    } catch (err) {
+        res.status(500)
+        console.error(err)
+    }
+})
+
+
+//Returns percentage of favFood
+router.get('/:favFood', async (req, res) => {
+    try {
+        let hamstersArray = [];
+        let favFoodArray = [];
+        let hamsterDB = await db.collection('hamsters')
+            .where("favFood", "==", req.params.favFood).get();
+
+        hamsterDB.forEach(favFood => {
+            favFoodArray.push(favFood.data());
+        })
+
+        let hamsterTotal = await db.collection('hamsters').get();
+
+        hamsterTotal.forEach(total => {
+            hamstersArray.push(total.data());
+        })
+
+
+        res.status(200).send({ Total: hamstersArray.length, favFood: favFoodArray.length, Percentag: (favFoodArray.length / hamstersArray.length) * 100 + "%" });
+
+    } catch (err) {
+        res.status(500)
+        console.error(err)
+    }
+})
+
 
 module.exports = router;
